b348810f7f93e81d2c467347aae36fe1fc75c7a2
Aaron Hicks <hicksa@landcareresearch.co.nz>
2014-07-08 16:12:57 +1200

    Checking all the parameters are passed through.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -29,11 +29,75 @@ describe 'gitlab', :type => :class do
         'db_user'             => 'git',
         'db_name'             => 'gitlab',
         'db_user_password'    => 'veryveryunsafe',
-        'db_user_passwd_hash' => $db_user_passwd_hash,
+        'db_user_passwd_hash' => nil,
         'gitlab_server'       => 'test.example.org',
-        'db_host'             => 'test.example.org',
+        'db_host'             => 'test.example.org'
       )}
     end
+    describe 'when not managing the gitlab shell install' do
+      let :params do
+        {
+          :install_gl_shell => false
+        }
+      end
+      it { should_not contain_class('gitlab::shell::install') }
+    end
+    describe 'when not managing the gitlab database install' do
+      let :params do
+        {
+          :manage_db => false
+        }
+      end
+      it { should_not contain_class('gitlab::db::postgresql') }
+    end
+    describe 'when given a user name and home directory' do
+      let :params do
+        {
+          :user       => 'nobody',
+          :user_home  => '/path/to/home'
+        }
+      end
+      it { should contain_user('gitlab').with(
+        'name'  => 'nobody',
+        'home'  => '/path/to/home'
+      ) }
+      it { should contain_class('gitlab::shell::install').with(
+        'user'      => 'nobody',
+        'user_home' => '/path/to/home'
+      ) }
+    end
+    describe 'when given a gitlab shell repository URL and revision' do
+      let :params do
+        {
+          :gitlab_shell_repo => 'https://git.example.org/repo.git',
+          :gitlab_shell_rev  => 'test'
+        }
+      end
+      it { should contain_class('gitlab::shell::install').with(
+        'repository'  => 'https://git.example.org/repo.git',
+        'revision'    => 'test'
+      ) }
+    end
+    describe 'when given database configuration details' do
+      let :params do
+        {
+          :db_user              => 'nobody',
+          :db_name              => 'gitlab_test',
+          :db_user_password     => 'stillinsecure',
+          :db_user_passwd_hash  => 'this is not really a hash',
+          :servername           => 'gitlab.example.com',
+        }
+      end
+      it { should contain_class('gitlab::db::postgresql').with(
+        'db_user'             => 'nobody',
+        'db_name'             => 'gitlab_test',
+        'db_user_password'    => 'stillinsecure',
+        'db_user_passwd_hash' => 'this is not really a hash',
+        'gitlab_server'       => 'gitlab.example.com',
+        'db_host'             => 'gitlab.example.com'
+      )}
+    end
+
   end
 
   context 'on a RedHat OS' do
--- spec/classes/shell_install_spec.rb
+++ spec/classes/shell_install_spec.rb
@@ -8,34 +8,89 @@ describe 'gitlab::shell::install', :type => :class do
         :fqdn           => 'test.example.org',
       }
     end
-    let :pre_condition do
-      'include gitlab'
-    end
-    describe 'with no parameters' do
-      it { should contain_class('gitlab::params') }
-      it { should contain_vcsrepo('gitlab-shell').with(
-        'ensure'    => 'present',
-        'path'      => '/home/git/gitlab-shell',
-        'provider'  => 'git',
-        'user'      => 'git',
-        'source'    => 'https://gitlab.com/gitlab-org/gitlab-shell.git',
-        'revision'  => 'v1.8.0',
-        'require'   => 'User[gitlab]'
-      ) }
-      it { should contain_file('gitlab-shell-config').with(
-        'ensure'  => 'file',
-        'path'    => '/home/git/gitlab-shell/config.yml',
-        'owner'   => 'git',
-        'group'   => 'git',
-        'require' => 'Vcsrepo[gitlab-shell]'
-      )}
-      it { should contain_exec('gitlab_shell_install').with(
-        'cwd'         => '/home/git',
-        'user'        => 'git',
-        'command'     => '/home/git/gitlab-shell/bin/install',
-        'subscribe'   => 'File[gitlab-shell-config]',
-        'refreshonly' => true
-      ) }
+    describe 'with default gitlab (disable shell install so test can redeclare)' do
+      let :pre_condition do
+        "class {'gitlab': install_gl_shell => false }"
+      end
+      describe 'with no parameters' do
+        it { should contain_class('gitlab::params') }
+        it { should contain_vcsrepo('gitlab-shell').with(
+          'ensure'    => 'present',
+          'path'      => '/home/git/gitlab-shell',
+          'provider'  => 'git',
+          'user'      => 'git',
+          'source'    => 'https://gitlab.com/gitlab-org/gitlab-shell.git',
+          'revision'  => 'v1.8.0',
+          'require'   => 'User[gitlab]'
+        ) }
+        it { should contain_file('gitlab-shell-config').with(
+          'ensure'  => 'file',
+          'path'    => '/home/git/gitlab-shell/config.yml',
+          'owner'   => 'git',
+          'group'   => 'git',
+          'require' => 'Vcsrepo[gitlab-shell]'
+        )}
+        it { should contain_exec('gitlab_shell_install').with(
+          'cwd'         => '/home/git',
+          'user'        => 'git',
+          'command'     => '/home/git/gitlab-shell/bin/install',
+          'subscribe'   => 'File[gitlab-shell-config]',
+          'refreshonly' => true
+        ) }
+      end
+      describe 'when given a user' do
+        let :params do
+          {
+            :user => 'notgit'
+          }
+        end
+        it { should contain_vcsrepo('gitlab-shell').with(
+          'user'     => 'notgit'
+        ) }
+        it { should contain_file('gitlab-shell-config').with(
+          'owner'   => 'notgit',
+          'group'   => 'notgit'
+        )}
+        it { should contain_exec('gitlab_shell_install').with(
+          'user'    => 'notgit'
+        ) }
+      end
+      describe 'when given a user home directory' do
+        let :params do
+          {
+            :user_home => '/path/to/home'
+          }
+        end
+        it { should contain_vcsrepo('gitlab-shell').with(
+          'path'     => '/path/to/home/gitlab-shell'
+        ) }
+        it { should contain_file('gitlab-shell-config').with(
+          'path'    => '/path/to/home/gitlab-shell/config.yml'
+        )}
+        it { should contain_exec('gitlab_shell_install').with(
+          'cwd'         => '/path/to/home'
+        ) }
+      end
+      describe 'when given an alternative repository' do
+        let :params do
+          {
+            :repository => 'https://git.example.org/repo.git'
+          }
+        end
+        it { should contain_vcsrepo('gitlab-shell').with(
+          'source'    => 'https://git.example.org/repo.git'
+        ) }
+      end
+      describe 'when given a repository reference' do
+        let :params do
+          {
+            :revision => 'test'
+          }
+        end
+        it { should contain_vcsrepo('gitlab-shell').with(
+          'revision'  => 'test'
+        ) }
+      end
     end
   end
 
@@ -53,7 +108,7 @@ describe 'gitlab::shell::install', :type => :class do
     end
   end
 
-    context 'on an Unknown OS' do
+  context 'on an Unknown OS' do
     let :facts do
       {
         :osfamily       => 'Unknown',
@@ -66,5 +121,4 @@ describe 'gitlab::shell::install', :type => :class do
       }.to raise_error(Puppet::Error, /The GitLab Puppet module does not support Unknown family of operating systems/)
     end
   end
-
 end
